28ecd1c2dac5b15fa01a0d73e223cbe5796fadf3
davyuan <davyuan@gmail.com>
2025-10-26 22:47:06 -0500

    debug

--- config/train_gpt2_xl.py
+++ config/train_gpt2_xl.py
@@ -8,9 +8,9 @@ wandb_run_name='gpt2-1558M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 1
+batch_size = 4
 block_size = 1024
-gradient_accumulation_steps = 16
+gradient_accumulation_steps = 4
 
 # eval stuff
 eval_interval = 30
